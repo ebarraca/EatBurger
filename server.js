b73f4a9d9ae4c0123dbee837bb4b933b119026cf
Erica Barraca <ericabarraca@dhcp-10-105-57-185.wireless.northwestern.private>
2018-03-12 18:38:58 -0500

    mysql connection

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require ("body-parser");
 var exphbs = require("express-handlebars");
 var mysql = require ("mysql");
+var connection = require ("./config/connection.js")
 
 var app = express();
 app.use(bodyParser.urlencoded({
@@ -14,33 +15,33 @@ app.set("view engine", "handlebars");
 var port = 3000;
 app.listen(port);
 
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: "",
-    database: "burgers_db"
-  });
-
-  connection.connect(function(err){
-      if(err)throw err;
-      console.log('Connected as id: '+connection.threadId);
-  })
-
   // Use Handlebars to render the main index.html page with the burgers in it.
   app.get("/", function(req, res) {
-    connection.query("SELECT * FROM burgers", function(err, data) {
-      if (err) {
-        return res.status(500).end();
-      }
+    connection.query("SELECT * FROM burgers;", function(err, data) {
+      if (err) throw err;
+
+      // Test it
+      // console.log('The solution is: ', data);
+
+      // Test it
+      // return res.send(data);
 
       res.render("index", { burgers: data });
     });
   });
 
   // Create a new burger
-  app.post('/create', function(req, res) {
+  app.post('/', function(req, res) {
     connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [req.body.burger], function(err, result) {
         console.log(req.body.burger);
+          // Test it
+          // console.log('You sent, ' + req.body.task);
+
+          // Test it
+          // return res.send('You sent, ' + req.body.task);
+            res.redirect("/");
+          // });
+        });
 
       // if (err) {
       //   res.redirect('/');
@@ -51,4 +52,4 @@ var connection = mysql.createConnection({
       // res.json({ id: result.insertId });
       // console.log({ id: result.insertId });
     });
-  });
+  // });
